e236aac2a8f65793ee32d74e6d81a680a89494b9
Akash Jaiswal <akashjais929@gmail.com>
2025-02-09 00:43:56 +0530

    Added new services

--- init-multiple-dbs.sql
+++ init-multiple-dbs.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE IF NOT EXISTS userservice;
 CREATE DATABASE IF NOT EXISTS movieservice;
+CREATE DATABASE IF NOT EXISTS reviewservice;
+CREATE DATABASE IF NOT EXISTS ratingservice;
 
 CREATE USER IF NOT EXISTS 'culeAkash'@'%' IDENTIFIED BY 'Akashjais@929';
 
 GRANT ALL PRIVILEGES ON userservice.* TO 'culeAkash'@'%';
 GRANT ALL PRIVILEGES ON movieservice.* TO 'culeAkash'@'%';
+GRANT ALL PRIVILEGES ON reviewservice.* TO 'culeAkash'@'%';
+GRANT ALL PRIVILEGES ON ratingservice.* TO 'culeAkash'@'%';
 
 FLUSH PRIVILEGES;
